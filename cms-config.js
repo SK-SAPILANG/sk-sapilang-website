3e240725e6413e9e4b3eaf4c4c61523c745f075f
SK Sapilang <sksapilangbacnotanlaunion@gmail.com>
2026-09-03 17:11:57 +0800

    Fix visitor logbook configuration

--- cms-config.js
+++ cms-config.js
@@ -1,2 +1,2 @@
-/* Paste your deployed Google Apps Script Web App URL between the quotation marks. */
-window.SK_CMS_ENDPOINT = "";
+/* SK Sapilang deployed Google Apps Script Web App. */
+window.SK_CMS_ENDPOINT = "https://script.google.com/macros/s/AKfycbxG-b_P47JMJu-S8AvU-Az-tZFJiGhxs9IzKrPCZgxTJwdI7Se2bbyx0z6DKvYnZ90Jqg/exec";
